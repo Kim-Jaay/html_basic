56cd9e9448a3290b484ef39ea26e501b6036768c
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-08-04 14:41:00 +0900

    0804

---
+++ project/src/doc/Section03.js
@@ -0,0 +1,44 @@
+const Section03 = () => {
+    return (
+        <section className="business02 basicSection">
+            <h2><span>OUR</span> BUSINESS</h2>
+            <p>Korea No.1 캐릭터, 카카오프렌즈</p>
+
+            <div className="container">
+                <div className="left">
+                    <figure>
+                        <img src="./img/DM_20220801165606_007.jpg" alt="" />
+                    </figure>
+                </div>
+                <div className="right">
+                    <h3>심플하지만 다 갖춘 jay테마</h3>
+                    <p>카카오프렌즈는 글로벌 캐릭터로 나아가고 있습니다.</p>
+
+                    <ul className="dotList">
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio?</li>
+                        <li>Lorem ipsum dolor sit amet consectetur.</li>
+                    </ul>
+
+                    <div className="bottom">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit, Optio.<br />
+                        sir sit amet consectetur.
+                    </div>
+                </div>
+            </div>
+            <div className="inner">
+                <h3>카카오프렌즈는 글로벌 캐릭터</h3>
+                <ul className="row">
+                    <li data-num="01">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+                    <li data-num="02">Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                    <li data-num="03">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio?</li>
+                    <li data-num="04">Lorem ipsum dolor sit amet consectetur.</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+
+export default Section03;
